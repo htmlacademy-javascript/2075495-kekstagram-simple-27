2b54cfc187684eb8ca096f744be746591cee9800
muskratysh <muskratysh@gmail.com>
2022-10-15 21:21:46 +0300

    Разбивает код js на модули

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+import {getPhotos} from './rendering.js'; // модуль с функцией для генерации данных
 
-const getRandomNumber = (min, max) => {
-  if (min < 0 || max < 0 || min >= max) {
-    return NaN;
-  }
-  const randomNumber = Math.round(Math.random() * (max - min) + min);
-  return randomNumber;
-};
-
-getRandomNumber(2,5);
-
-// Функция для проверки максимальной длины строки
-
-const isStringLengthValid = (string, maxLength) => string.length < maxLength;
-
-isStringLengthValid ('Функция для проверки максимальной длины строки', 5);
+console.log(
+  getPhotos()
+);
