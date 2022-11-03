89d000f8ed4a1ab8fbe984dc4c62b3be9230b5a5
muskratysh <muskratysh@gmail.com>
2022-11-03 16:53:33 +0300

    Исправляет ошибки в домашке

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-import {getPhotos} from './createphotos.js'; // модуль с функцией для генерации фотокарточек
-import {renderPictures} from 'rendering.js';
-renderPictures(getPhotos());
+import {getPhotos} from './get-photos.js'; // модуль с функцией для генерации фотокарточек
+import {renderPhotos} from './rendering.js';
+
+const photos = getPhotos();
+renderPhotos(photos);
