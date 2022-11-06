6d5f37e7383af1cf66042fcf26f78d0675a9a454
muskratysh <muskratysh@gmail.com>
2022-11-06 13:55:40 +0300

    Исправляет ошибки в ДЗ

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {getPhotos} from './get-photos.js'; // модуль с функцией для генерации фотокарточек
 import {renderPhotos} from './rendering.js';
-import { initForm } from './init-form.js';
+import { initForm } from './form.js';
 
 const photos = getPhotos();
 renderPhotos(photos);
