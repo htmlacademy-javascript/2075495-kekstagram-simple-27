fd8af6a1d4513377d5eab5b2927a4dc273833c74
muskratysh <muskratysh@gmail.com>
2022-11-11 00:18:49 +0300

    Добавляет изменение масштаба

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {getPhotos} from './get-photos.js'; // модуль с функцией для генерации фотокарточек
 import {renderPhotos} from './rendering.js';
 import { initForm } from './form.js';
+import { changeSize } from './scale-change.js';
 
 const photos = getPhotos();
 renderPhotos(photos);
 initForm();
+changeSize();
